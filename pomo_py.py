d349569127d983756ed6c3324527a3d66ae481f1
Bhavyesh vyas <vyasbhavyesh@gmail.com>
2018-07-12 18:37:28 +0530

    git test

--- pomo_py.py
+++ pomo_py.py
@@ -9,6 +9,6 @@ import tkinter as Tk
 win = Tk.Tk()
 win.geometry('1080x720')
 win.title("Welcome to LikeGeeks app")
-lbl = Tk.Label(win, text="Har Har Mahadev", font=("Arial Bold", 50))
+lbl = Tk.Label(win, text="Har Har Mahadev Sambho!", font=("Arial Bold", 50))
 lbl.grid(column=0, row=0)
 win.mainloop()
